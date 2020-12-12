1d49320145e35d0e05209dca318e7af06ef3b1da
lemon <zjc2310@foxmail.com>
2020-12-12 11:16:19 +0800

    feat:平台属性管理页面-静态页面完成

--- src/views/product/attr/list.vue
+++ src/views/product/attr/list.vue
@@ -1,11 +1,73 @@
 <template>
-<div>
-  平台属性
-</div>
+  <div>
+    <el-card>
+      <el-form :inline="true" :model="category" class="demo-form-inline">
+        <el-form-item label="一级分类">
+          <el-select v-model="category.category1Id" placeholder="请选择">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="二级分类">
+          <el-select v-model="category.category2Id" placeholder="请选择">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="三级分类">
+          <el-select v-model="category.category3Id" placeholder="请选择">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card style="margin-top: 20px">
+      <el-button style="margin-bottom: 20px" type="primary"
+        >+添加属性</el-button
+      >
+      <el-table :data="trademarkList" style="width: 100%" border>
+        <el-table-column type="index" label="序号" width="80" align="center">
+        </el-table-column>
+        <el-table-column prop="tmName" label="属性名称" width="150">
+        </el-table-column>
+        <el-table-column label="属性值列表">
+          <template slot-scope="scope">
+            <img class="trademark-logo" :src="scope.row.logoUrl" alt="logo" />
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="150">
+          <template slot-scope="scope">
+            <el-button
+              type="warning"
+              icon="el-icon-edit"
+              @click="editTrademark(scope.row)"
+              >修改</el-button
+            >
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              @click="delTrademark(scope.row)"
+              >删除</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'AttrList',
-}
+  name: "AttrList",
+  data() {
+    return {
+      category: {
+        category1Id: "",
+        category2Id: "",
+        category3Id: "",
+      },
+    };
+  },
+};
 </script>
